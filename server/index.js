4c94b5adede36cf66e380d767476a80a7fe8638f
Pavel Lauko <pavel.lauko@bsc-ideas.com>
2022-04-01 15:25:25 +0200

    Query for single portfolio

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ app.prepare().then(() => {
         }
         type Query {
             hello: String
-            portfolio: Portfolio
+            portfolio(id: ID): Portfolio
             portfolios: [Portfolio]
         }
     `);
@@ -76,8 +76,9 @@ app.prepare().then(() => {
   // the root provides a resolver for each API endpoint
   const root = {
     hello: () => 'Hello world',
-    portfolio: () => {
-      return data.portfolios[0]
+    portfolio: ({ id }) => {
+      const portfolio = data.portfolios.find((portfolio) => portfolio._id === id)
+      return portfolio
     },
     portfolios: () => {
       return data.portfolios
